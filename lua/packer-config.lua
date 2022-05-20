bdaaa269fdb975c00682f5796a80225a383f3847
Peter <peterthomaskim@gmail.com>
2022-05-20 20:18:45 -0300

    lua config

---
+++ lua/packer-config.lua
@@ -0,0 +1,8 @@
+return require'packer'.startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'ellisonleao/gruvbox.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'neovim/nvim-lspconfig'
+    use 'itchyny/lightline.vim'
+end)
