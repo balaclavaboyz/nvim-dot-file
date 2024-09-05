0c184ec3e86d3182abc69144df59c189359644bc
peter <peterthomaskim@gmail.com>
2024-09-05 09:39:34 -0300

    cmp

--- init.lua
+++ init.lua
@@ -139,10 +139,67 @@ vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEn
 })
 ---ENDWORKAROUND
 
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+      -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+})
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require("mason").setup()
 require('mason-lspconfig').setup()
-require'lspconfig'.lua_ls.setup{}
+require'lspconfig'.lua_ls.setup({
+  capabilities=capabilities
+})
 require'lspconfig'.pylsp.setup{
+  capabilities=capabilities,
   settings = {
     pylsp = {
       plugins = {
@@ -154,8 +211,6 @@ require'lspconfig'.pylsp.setup{
     }
   }
 }
-require'lspconfig'.ruff.setup{}
-require'lspconfig'.ruff_lsp.setup{}
 require 'nvim-treesitter.install'.compilers = { "clang" }
 
 local builtin = require('telescope.builtin')
@@ -176,7 +231,6 @@ return require('packer').startup(function(use)
   use'williamboman/mason.nvim'
   use'williamboman/mason-lspconfig.nvim'
   use'neovim/nvim-lspconfig'
-  use'hrsh7th/nvim-cmp'
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function()
@@ -188,6 +242,11 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag='0.1.x',
     requires={{'nvim-lua/plenary.nvim'}}
   }
+  use'hrsh7th/cmp-nvim-lsp'
+  use'hrsh7th/cmp-buffer'
+  use'hrsh7th/cmp-path'
+  use'hrsh7th/cmp-cmdline'
+  use'hrsh7th/nvim-cmp'
 end)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
