4788d0fc6e66ad7afbf44fce00f44473bdcd8759
Peter Kim <peterthomaskim@gmail.com>
2025-03-14 08:56:19 -0300

    add the rest of disabled lint

--- init.lua
+++ init.lua
@@ -705,8 +705,12 @@ require('lazy').setup({
               plugins = {
                 pyflakes = { enabled = false },
                 pycodestyle = { enabled = false },
-                pylint = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
                 mccabe = { enabled = false },
+                pylsp_mypy = { enabled = false },
+                pylsp_black = { enabled = false },
+                pylsp_isort = { enabled = false },
               },
             },
           },
