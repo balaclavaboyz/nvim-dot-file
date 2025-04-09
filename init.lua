2fc18b752dbbc8bb7d053302e0a23a5c1850fcb1
Peter Kim <peterthomaskim@gmail.com>
2025-04-09 14:04:50 -0300

    silence warnings

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
+-- Silence the specific position encoding message
+local notify_original = vim.notify
+vim.notify = function(msg, ...)
+  if
+    msg
+    and (
+      msg:match 'position_encoding param is required'
+      or msg:match 'Defaulting to position encoding of the first client'
+      or msg:match 'multiple different client offset_encodings'
+    )
+  then
+    return
+  end
+  return notify_original(msg, ...)
+end
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
