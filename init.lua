c6ddecaa8dafcf06f0d8ffeae28c7c867adc723b
Peter Kim <peterthomaskim@gmail.com>
2025-04-09 13:52:56 -0300

    gruvbox theme

--- init.lua
+++ init.lua
@@ -1,91 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
@@ -232,47 +144,6 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 --
-vim.keymap.set('n', '<F5>', function()
-  require('dap').continue()
-end)
-vim.keymap.set('n', '<F10>', function()
-  require('dap').step_over()
-end)
-vim.keymap.set('n', '<F11>', function()
-  require('dap').step_into()
-end)
-vim.keymap.set('n', '<F12>', function()
-  require('dap').step_out()
-end)
-vim.keymap.set('n', '<Leader>b', function()
-  require('dap').toggle_breakpoint()
-end)
-vim.keymap.set('n', '<Leader>B', function()
-  require('dap').set_breakpoint()
-end)
-vim.keymap.set('n', '<Leader>lp', function()
-  require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
-end)
-vim.keymap.set('n', '<Leader>dr', function()
-  require('dap').repl.open()
-end)
-vim.keymap.set('n', '<Leader>dl', function()
-  require('dap').run_last()
-end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-  require('dap.ui.widgets').hover()
-end)
-vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-  require('dap.ui.widgets').preview()
-end)
-vim.keymap.set('n', '<Leader>df', function()
-  local widgets = require 'dap.ui.widgets'
-  widgets.centered_float(widgets.frames)
-end)
-vim.keymap.set('n', '<Leader>ds', function()
-  local widgets = require 'dap.ui.widgets'
-  widgets.centered_float(widgets.scopes)
-end)
 require('lazy').setup({
   { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } },
   {
@@ -283,6 +154,15 @@ require('lazy').setup({
     config = function()
       local dap = require 'dap'
       local ui = require 'dapui'
+      dap.adapters.python = {
+        type = 'executable',
+        command = 'pythonw.exe', -- Ajuste o caminho para o seu pythonw.exe
+        args = { '-m', 'debugpy.adapter' },
+        options = {
+          -- No Windows, usa CREATE_NO_WINDOW para evitar a janela de console
+          creationflags = 0x08000000, -- CREATE_NO_WINDOW
+        },
+      }
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
       end
@@ -295,14 +175,64 @@ require('lazy').setup({
       dap.listeners.before.event_exited.dapui_config = function()
         ui.close()
       end
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          console = 'internalConsole', -- Sem console visível
+        },
+      }
+      vim.keymap.set('n', '<F5>', function()
+        dap.continue()
+      end)
+      vim.keymap.set('n', '<F10>', function()
+        dap.step_over()
+      end)
+      vim.keymap.set('n', '<F11>', function()
+        dap.step_into()
+      end)
+      vim.keymap.set('n', '<F12>', function()
+        dap.step_out()
+      end)
+      vim.keymap.set('n', '<Leader>b', function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set('n', '<Leader>B', function()
+        dap.set_breakpoint()
+      end)
+      vim.keymap.set('n', '<Leader>lp', function()
+        dap.set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+      end)
+      vim.keymap.set('n', '<Leader>dr', function()
+        dap.repl.open()
+      end)
+      vim.keymap.set('n', '<Leader>dl', function()
+        dap.run_last()
+      end)
+      vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+        dap.ui.widgets.hover()
+      end)
+      vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+        dap.ui.widgets.preview()
+      end)
+      vim.keymap.set('n', '<Leader>df', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.frames)
+      end)
+      vim.keymap.set('n', '<Leader>ds', function()
+        local widgets = require 'dap.ui.widgets'
+        widgets.centered_float(widgets.scopes)
+      end)
     end,
   },
   {
     'mfussenegger/nvim-dap-python',
     config = function()
-      local bomdia = require 'dap-python'
-      bomdia.setup 'python'
+      require('dap-python').setup 'pythonw.exe'
     end,
+    lazy = true,
   },
 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
@@ -342,6 +272,7 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+    lazy = true,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -413,6 +344,7 @@ require('lazy').setup({
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
+    lazy = true,
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -891,6 +823,7 @@ require('lazy').setup({
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
+    lazy = true,
   },
   -- { -- Autocompletion
   --   'hrsh7th/nvim-cmp',
@@ -1010,27 +943,33 @@ require('lazy').setup({
   --   end,
   -- },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'morhetz/gruvbox',
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   config = function()
+  --     ---@diagnostic disable-next-line: missing-fields
+  --     require('tokyonight').setup {
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+  --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -1071,6 +1010,7 @@ require('lazy').setup({
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
+    lazy = true,
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
